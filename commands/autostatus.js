bd9e5b29497893ff119fe1c03a6e06f9b5055d18
Chris Gaaju <chrisgaajukpeme2027@gmail.com>
2026-06-19 18:43:45 +0100

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -22,7 +22,7 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363420618370733@newsletter',
+            newsletterJid: '120363406588763460@newsletter',
             newsletterName: 'GAAJU-XMD BOTS',
             serverMessageId: -1
         }
