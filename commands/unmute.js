7e938382cfd04d72ac2e6d4ab83de978f5aa1207
Chris Gaaju <chrisgaajukpeme2027@gmail.com>
2026-06-23 10:04:00 +0100

    Delete commands directory

--- commands/unmute.js
+++
@@ -1,6 +0,0 @@
- async function unmuteCommand(sock, chatId) {
-    await sock.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
-    await sock.sendMessage(chatId, { text: '*🔴This group has been successfully unmuted🔴.*' });
-}
-
-module.exports = unmuteCommand;
